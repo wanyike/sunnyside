c30590b98a1de503d5b806365e0d1a15c307ba39
wanyike <wanyikeemmanuel@gmail.com>
2024-11-10 20:34:53 +0300

    modified graphic.js

--- src/data/graphic.js
+++ src/data/graphic.js
@@ -18,12 +18,12 @@ export const GraphicData = [
         id: 1,
         image: "./images/desktop/image-graphic-design.jpg",
         title: "Graphic Design",
-        Text: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
+        text: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
     },
     {
         id: 2,
         image: "./images/desktop/image-photography.jpg",
         title: "Photography",
-        Text: " Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+        text: " Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
     }
 ];
