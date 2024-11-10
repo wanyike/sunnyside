177812581058c53f884f637e7b0130b34797d055
wanyike <wanyikeemmanuel@gmail.com>
2024-11-10 23:39:17 +0300

    feat

---
+++ src/data/testimonials.js
@@ -0,0 +1,23 @@
+export const testimnials = [
+    {
+        id: 1,
+        name: "Emily R.",
+        about: " We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+        title: "Marketing Director"
+    },
+
+    {
+        id: 2,
+        name: "Thomas S.",
+        about: "Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.",
+        title: "Chief Operating Officer"
+    },
+
+    {
+        id: 3,
+        name: "Jennie F.",
+        about: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+        title: "Business Owner"
+   } ,
+]
+
