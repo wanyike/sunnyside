d17b9117a2805f68e6897ca964370017b6ff54e1
wanyike <wanyikeemmanuel@gmail.com>
2024-11-12 22:45:28 +0300

    feat

---
+++ src/data/grid.js
@@ -0,0 +1,21 @@
+export const gridMobile = [
+    {
+        id: 1,
+        image: "./images/mobile/image-milkbottles.jpg"
+    },
+   
+    {
+        id: 2,
+        image: "./images/mobile/image-orange.jpg"
+    },
+
+    {
+        id: 3,
+        image: "./images/mobile/image-cone.jpg"
+    },
+
+    {
+        id: 4,
+        image: "./images/mobile/image-sugar.jpg"
+    },
+]
