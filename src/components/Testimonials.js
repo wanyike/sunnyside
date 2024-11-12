74822703dddc5981d7be9aca0c13bc92e6defb61
wanyike <wanyikeemmanuel@gmail.com>
2024-11-12 22:33:32 +0300

    feat

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -21,12 +21,7 @@ const Testimonials = () => {
                       </article>
                   ))}
               </div>
-
-              
-              
-
-
-          </section>
+         </section>
       </>
   )
 }
